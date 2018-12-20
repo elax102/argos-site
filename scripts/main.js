10efa6fa5fd5ac0cf2c3bd819ab5585efb2e9ee0
Alex <davis.alex.jr@gmail.com>
2018-12-20 18:00:24 -0500

    Added heartbeat

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,43 @@
 new WOW().init();
+
+var vignette = document.getElementById("vignette");
+console.log(vignette);
+
+var waypoint = new Waypoint({
+	element: document.getElementById('sect1'),
+	handler: function () {
+		console.log('sect1');
+		vignette.className = "";
+	},
+	offset: -200
+});
+
+var waypoint = new Waypoint({
+	element: document.getElementById('sect2'),
+	handler: function () {
+		console.log('sect2');
+		vignette.className = "";
+		vignette.classList.add("pulse1");
+	},
+	offset: 300
+});
+
+var waypoint = new Waypoint({
+	element: document.getElementById('sect3'),
+	handler: function () {
+		console.log('sect3');
+		vignette.className="";
+		vignette.classList.add("pulse2");
+	},
+	offset: 300
+});
+
+var waypoint = new Waypoint({
+	element: document.getElementById('sect4'),
+	handler: function () {
+		console.log('sect4');
+		vignette.className = "";
+		vignette.classList.add("pulse3");
+	},
+	offset: 200
+});
